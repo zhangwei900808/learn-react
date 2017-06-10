25a669f30f86a4a89f66efe700c43c7c9b9df9d9
张威 <zw900808@gmail.com>
2017-06-10 16:06:35 +0800

    update FilterProductTable.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,17 @@ import Reservation from './components/Reservation'
 import Calculator from './components/Calculator'
 import WelcomeDialog from './components/WelcomeDialog'
 import Apps from './components/App'
+import FilterableProductTable from './components/FilterableProductTable'
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Apps />,document.getElementById('root'));
+ReactDOM.render(<FilterableProductTable products={PRODUCTS}/>,document.getElementById('root'));
 registerServiceWorker();
